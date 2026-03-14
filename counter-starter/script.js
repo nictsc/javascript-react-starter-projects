458563b394daaa975d2ab4b3fc2802c7e16f59a5
Nicole Chan <nictscnotes@gmail.com>
2026-03-15 10:34:07 +1100

    Added comments to explain what script.js in counter do

--- counter-starter/script.js
+++ counter-starter/script.js
@@ -1,28 +1,35 @@
-function add(value) { 
-    let numberElement = document.getElementById("number")
-    let number = parseInt(numberElement.innerText);
-    number = number + value;
-    numberElement.innerText = number;
+// value is the argument that contains the specific number we wish to update the number with the mathematical operation.
+function add(value) {
+  // Giving the number element an ID number for further access
+  let numberElement = document.getElementById("number");
+  // Using innerText property to access text in the element. Convert type of number variable from string to integer.
+  let number = parseInt(numberElement.innerText);
+  // Adding number to be displayed by the specified value
+  number = number + value;
+  // Assigning text in number element to show the updated number
+  numberElement.innerText = number;
 }
 
+// value is the argument that contains the specific number we wish to update the number with the mathematical operation.
 function subtract(value) {
-    let numberElement = document.getElementById("number")
-    let number = parseInt(numberElement.innerText);
-    number = number - value;
-    numberElement.innerText = number;
+  // Giving the number element an ID number for further access
+  let numberElement = document.getElementById("number");
+  // Using innerText property to access text in the element. Convert type of number variable from string to integer.
+  let number = parseInt(numberElement.innerText);
+  // Subtracting number to be displayed by the specified value
+  number = number - value;
+  // Assigning text in number element to show the updated number
+  numberElement.innerText = number;
 }
 
+// value is the argument that contains the specific number we wish to update the number with the mathematical operation.
 function reset(value) {
-    let numberElement = document.getElementById("number")
-    let number = parseInt(numberElement.innerText);
-    number = 0
-    numberElement.innerText = number;
-}
-
-function emoji(value) {
-    let numberElement = document.getElementById("number")
-    let number = parseInt(numberElement.innerText);
-    if (number > 10) {
-        console.log()
-    }
+  // Giving the number element an ID number for further access
+  let numberElement = document.getElementById("number");
+  // Using innerText property to access text in the element. Convert type of number variable from string to integer.
+  let number = parseInt(numberElement.innerText);
+  // Reassigning value to number
+  number = 0;
+  // Assigning text in number element to show the updated number
+  numberElement.innerText = number;
 }
